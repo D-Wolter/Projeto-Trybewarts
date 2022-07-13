9447001272468721e377ddc349d342bfcf20cd46
Daniel Wolter <wolter.daniel@gmail.com>
2022-07-13 16:42:26 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const buttonHeader = document.getElementById('button-header');
-const email = document.getElementById('email').innerText;
-const senha = document.getElementById('senha').innerText;
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
 
-function validarFormsHeader (e) {
-    e.preventDefault();
-    const valor = email
-    console.log(valor);
+function validarFormsHeader(e) {
+  e.preventDefault();
+  const userEmail = email.value;
+  const userSenha = senha.value;
+  if (userEmail === 'tryber@teste.com' && userSenha === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
 }
-buttonHeader.addEventListener('click', validarFormsHeader)
+buttonHeader.addEventListener('click', validarFormsHeader);

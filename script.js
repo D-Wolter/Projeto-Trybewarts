2196337be265000debc67aec10da6f6d5d6d5462
Daniel Wolter <wolter.daniel@gmail.com>
2022-07-14 13:14:42 -0300

    criando funcao submit

--- script.js
+++ script.js
@@ -34,3 +34,63 @@ function refresh() {
 }
 
 textLen.addEventListener('keyup', refresh);
+
+const nome = document.getElementById('input-name');
+const sobrenome = document.getElementById('input-lastname');
+const eMail = document.getElementById('input-email');
+const casa = document.getElementById('house').value;
+const familia1 = document.getElementsByName('family')[0];
+const familia2 = document.getElementsByName('family')[1];
+const familia3 = document.getElementsByName('family')[2];
+const lang1 = document.getElementById('hofs');
+const lang2 = document.getElementById('jest');
+const lang3 = document.getElementById('promises');
+const lang4 = document.getElementById('react');
+const lang5 = document.getElementById('sql');
+const lang6 = document.getElementById('python');
+const n1 = document.getElementsByName('rate');
+
+function makeForm (e) {
+  e.preventDefault()
+  console.log(nome.value)
+  console.log(sobrenome.value)
+  console.log(eMail.value)
+  console.log(casa);
+
+  //tratar qual dos trez e verdadeiro
+  if (familia1.checked === true) {
+    console.log(familia1.value);
+  } 
+  if (familia2.checked === true) {
+    console.log(familia2.value);
+  } 
+  if (familia3.checked === true) {
+    console.log(familia3.value);
+  }
+  if (lang1.checked === true) {
+    console.log(lang1.value);
+  }
+  if (lang2.checked === true) {
+    console.log(lang2.value);
+  }
+  if (lang3.checked === true) {
+    console.log(lang3.value);
+  }
+  if (lang4.checked === true) {
+    console.log(lang4.value);
+  }
+  if (lang5.checked === true) {
+    console.log(lang5.value);
+  }
+  if (lang6.checked === true) {
+    console.log(lang6.value);
+  }
+  for (v of n1) {
+    if (v.checked === true) {
+      console.log(v.value.id);
+    }
+  }
+  
+}
+
+btn.addEventListener('click', makeForm)
